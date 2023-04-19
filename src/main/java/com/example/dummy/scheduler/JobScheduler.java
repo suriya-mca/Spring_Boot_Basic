bababc7b65a75fe48d9eed55e8d0986772c89d2e
Suriya <suriyag1999@gmail.com>
2023-04-19 09:47:41 +0530

    connected to postgres docker container

--- src/main/java/com/example/dummy/scheduler/JobScheduler.java
+++ src/main/java/com/example/dummy/scheduler/JobScheduler.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Component;
 
 // @Component
 @EnableAsync
